2f0b27acc37b129868952e2881afb99fc05361a0
hhuan19981212 <9527@163.com>
2021-02-25 23:42:27 +0800

    前端模板

---
+++ src/main/java/com/hhuan/adminlte/AdminlteApplication.java
@@ -0,0 +1,13 @@
+package com.hhuan.adminlte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminlteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminlteApplication.class, args);
+	}
+
+}
